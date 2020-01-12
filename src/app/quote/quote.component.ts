053f8994e5e68300fb3b23a2ba81c53b2c87af63
ian mwangi <iankimotho233@gmail.com>
2020-01-12 22:16:09 +0300

    Added a new quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,8 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
     new Quote(1,"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.","Marilyn Monroe","Ian Mwangi",new Date(2019,4,6)),
-    new Quote(2,"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.","Bernard M. Baruch","John",new Date(2019,5,20))
+    new Quote(2,"Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.","Bernard M. Baruch","John",new Date(2019,5,20)),
+    new Quote(3,"I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.","Maya Angelou","Peter Parker",new Date(2017,4,10))
   ];
 
 
